a4dd9de998477f3a23869265fa19517e295507fc
napodan <david.guenet@gmail.com>
2012-10-11 01:52:10 +0200

    ev_bravo -> bravo

--- ev.mk
+++ ev.mk
@@ -1,6 +1,3 @@
-# Pull all dictionaries (This makes sure it gets called)
-TARGET_USE_KEYBOARD := international
-
 # Call this first so apn list is actually copied
 $(call inherit-product, vendor/ev/config/gsm.mk)
 
@@ -14,7 +11,7 @@ PRODUCT_LOCALES := sw320dp
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME    := ev_bravo
+PRODUCT_NAME    := bravo
 PRODUCT_BRAND   := htc_wwe
 PRODUCT_DEVICE  := bravo
 PRODUCT_MODEL   := HTC Desire
@@ -27,12 +24,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_bravo BUILD_FINGERPRINT=htc_wwe
 PRODUCT_CODENAME := Turba
 PRODUCT_VERSION_DEVICE_SPECIFIC := p12
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your HTC Desire\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing this rom based on Evervolv for your HTC Desire\nPlease visit evervolv at \#evervolv on irc.freenode.net\n------------------------------------------------\n"
 
-# Copy compatible bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/ev/prebuilt/wvga/media/bootanimation.zip:system/media/bootanimation.zip
-#    device/htc/passion/extras/bootanimation_passion_ics.zip:system/media/bootanimation.zip
-
-# Hot reboot
-PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/hot_reboot
